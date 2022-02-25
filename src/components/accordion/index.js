7fcca8ed1691fb237effe95a964f4c0eca47bd2b
maisha <maisha@askeddi.com>
2022-02-25 12:51:21 +0000

    fix: accodian had a bug which is fixed the variable Frame was missing in the styles folder

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,12 +1,5 @@
-import React, { useState, useContext, createContext } from "react";
-import {
-  Container,
-  Title,
-  Item,
-  Inner,
-  Header,
-  Body,
-} from "./styles/accordion";
+import React, { useState, useContext, createContext } from 'react';
+import { Container, Frame, Title, Item, Inner, Header, Body } from './styles/accordion';
 
 const ToggleContext = createContext();
 
@@ -20,6 +13,10 @@ export default function Accordion({ children, ...restProps }) {
 
 Accordion.Title = function AccordionTitle({ children, ...restProps }) {
   return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Frame = function AccordionFrame({ children, ...restProps }) {
+  return <Frame {...restProps}>{children}</Frame>;
 };
 
 Accordion.Item = function AccordionItem({ children, ...restProps }) {
@@ -53,7 +50,7 @@ Accordion.Body = function AccordionBody({ children, ...restProps }) {
   /* return toggleShow ? <Body {...restProps}>{children}</Body> : null; */
 
   return (
-    <Body className={toggleShow ? "open" : "closed"} {...restProps}>
+    <Body className={toggleShow ? 'open' : 'closed'} {...restProps}>
       <span>{children}</span>
     </Body>
   );
--- src/components/accordion/styles/accordion.js
+++ src/components/accordion/styles/accordion.js
@@ -1,8 +1,12 @@
-import styled from "styled-components/macro";
+import styled from 'styled-components/macro';
 
 export const Container = styled.div`
   display: flex;
   border-bottom: 8px solid #222;
+`;
+
+export const Frame = styled.div`
+  margin-bottom: 40px;
 `;
 
 export const Inner = styled.div`
