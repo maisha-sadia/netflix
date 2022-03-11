b97569e0321b3e158b9e82585c776962fbcb564e
maisha <maisha@askeddi.com>
2022-03-11 22:49:35 +0000

    feat: setting up the firebase§

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/compat/app";
+import "firebase/compat/firestore";
+import "firebase/compat/auth";
+import { seedDatabase } from "../seed";
+
+// Configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDMRuR9V3qItQO0z7mJx3opCVI3UTP5Tys",
+  authDomain: "netfilx-b513b.firebaseapp.com",
+  projectId: "netfilx-b513b",
+  storageBucket: "netfilx-b513b.appspot.com",
+  messagingSenderId: "1054083889518",
+  appId: "1:1054083889518:web:f9f0227681e92c111ea97f",
+};
+
+const Firebase = firebase.initializeApp(firebaseConfig);
+
+// This is done to export data to Firebase
+// seedDatabase(Firebase);
+
+export { Firebase };
